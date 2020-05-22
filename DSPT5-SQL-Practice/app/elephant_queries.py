f8edec2ccaf859e9202b94ed94b2565352beaa85
Jisha Obukwelu <jisha@obukwelu.com>
2020-05-22 17:07:22 -0400

    Creating new table and inserting data

--- DSPT5-SQL-Practice/app/elephant_queries.py
+++ DSPT5-SQL-Practice/app/elephant_queries.py
@@ -1,9 +1,12 @@
 # app/elephant_queries.py
 
 import os
+from dotenv import load_dotenv
 import psycopg2
 from psycopg2.extras import DictCursor
-from dotenv import load_dotenv
+from psycopg2.extras import execute_values
+import json
+import pandas as pd
 
 load_dotenv() #> loads contents of the .env file into the script's environment
 ### Connect to ElephantSQL-hosted PostgreSQL
@@ -27,5 +30,74 @@ for row in result:
     print(type(row))
     print(row)
 
+"""
+------------------------------------------------- INSERTING INFO TO DATABASE -------------------------------------------------
+"""
+#
+# CREATE THE TABLE
+#
 
+table_name = "test_table2"
 
+print("-------------------")
+query = f"""
+CREATE TABLE IF NOT EXISTS {table_name} (
+  id SERIAL PRIMARY KEY,
+  name varchar(40) NOT NULL,
+  data JSONB
+);
+"""
+print("SQL:", query)
+cursor.execute(query)
+
+#
+# INSERT SOME DATA
+#
+
+my_dict = { "a": 1, "b": ["dog", "cat", 42], "c": 'true' }
+
+# ONLY INSERTS ONE LINE AT A TIME
+#insertion_query = f"INSERT INTO {table_name} (name, data) VALUES (%s, %s)"
+#cursor.execute(insertion_query,
+#  ('A rowwwww', 'null')
+#)
+#cursor.execute(insertion_query,
+#  ('Another row, with JSONNNNN', json.dumps(my_dict))
+#)
+
+# h/t: https://stackoverflow.com/questions/8134602/psycopg2-insert-multiple-rows-with-one-query
+insertion_query = f"INSERT INTO {table_name} (name, data) VALUES %s"
+#execute_values(cursor, insertion_query, [
+#  ('A rowwwww', 'null'),
+#  ('Another row, with JSONNNNN', json.dumps(my_dict)),
+#  ('Third row', "3")
+#])
+
+df = pd.DataFrame([
+  ['A rowwwww', 'null'],
+  ['Another row, with JSONNNNN', json.dumps(my_dict)],
+  ['Third row', "null"],
+  ["Pandas Row", "null"]
+])
+
+records = df.to_dict("records") #> [{0: 'A rowwwww', 1: 'null'}, {0: 'Another row, with JSONNNNN', 1: '{"a": 1, "b": ["dog", "cat", 42], "c": "true"}'}, {0: 'Third row', 1: '3'}, {0: 'Pandas Row', 1: 'YOOO!'}]
+list_of_tuples = [(r[0], r[1]) for r in records]
+
+execute_values(cursor, insertion_query, list_of_tuples)
+
+#
+# QUERY THE TABLE
+#
+
+print("-------------------")
+query = f"SELECT * FROM {table_name};"
+print("SQL:", query)
+cursor.execute(query)
+for row in cursor.fetchall():
+    print(row)
+
+# ACTUALLY SAVE THE TRANSACTIONS
+connection.commit()
+
+cursor.close()
+connection.close()
